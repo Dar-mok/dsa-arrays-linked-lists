c31d2444bb9060f80db030e0bba0641b48bf74a9
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-06-12 14:55:51 -0700

    add pop and shift methods

--- linked-list.js
+++ linked-list.js
@@ -24,12 +24,12 @@ class LinkedList {
 
   push(val) {
     let newVal = new Node(val);
-    if (head === null){
-      this.head = newVal
-      this.tail = newVal
-    } else if (head){
+    if (head === null) {
+      this.head = newVal;
+      this.tail = newVal;
+    } else if (head) {
       this.tail.next = newVal;
-      this.tail = newVal
+      this.tail = newVal;
     }
     this.length++;
   }
@@ -40,12 +40,12 @@ class LinkedList {
     //account for if list is empty currently,
     let newVal = new Node(val);
 
-    if (head === null){
-      this.head = newVal
-      this.tail = newVal
-    //or if
-    } else if (head){
-      newVal.next =  this.head
+    if (head === null) {
+      this.head = newVal;
+      this.tail = newVal;
+
+    } else if (head) {
+      newVal.next = this.head;
       this.head = newVal;
     }
     this.length++;
@@ -54,13 +54,51 @@ class LinkedList {
   /** pop(): return & remove last item. */
 
   pop() {
+    let tailToRemove;
 
+    if (this.length === 0) {
+      throw new Error("List is empty.");
+    }
+
+    if (this.head === this.tail) {
+      tailToRemove = this.tail;
+      this.tail === null;
+      this.head === null;
+    } else {
+      for (let node of this) {
+        if (node.next === this.tail) {
+          tailToRemove = this.tail;
+          node.next = null;
+          this.tail = node;
+        }
+      }
+    }
+
+    this.length--;
+
+    return tailToRemove.val;
   }
 
   /** shift(): return & remove first item. */
 
   shift() {
+    let headToRemove;
 
+    if (this.length === 0) {
+      throw new Error("List is empty.");
+    }
+
+    if (this.head === this.tail) {
+      headToRemove = this.tail;
+      this.tail === null;
+      this.head === null;
+    } else {
+      headToRemove = this.head;
+      this.head = this.head.next;
+    }
+
+    this.length--;
+    return headToRemove;
   }
 
   /** getAt(idx): get val at idx. */
